a5f308e62afb60b31f8ee921768e74e3c215ed0a
Valerii Bosiak <vbosiak@scalhive.com>
2020-06-01 01:07:58 +0300

    Initial commit

---
+++ python_list_std_sort.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import time, array, numpy, sys
+
+def getDataStructure(size):
+    structure = sys.argv[1]
+    data = []
+    for _ in range(size):
+        data.append(int(input_data.readline()))
+    if structure == 'list':
+        return data
+    elif structure == 'array':
+        return array.array('i', data)
+    elif structure == 'np_array':
+        return numpy.array(data, dtype=int)
+
+input_data = open('input_data.txt', 'r')
+
+size = int(input_data.readline())
+
+input_data.close
+
+data = getDataStructure(size)
+print("Using {} for test".format(type(data)))
+print('{:,} integers have been loaded into memory'.format(len(data)))
+
+tic = time.time()
+sorted(data)
+toc = time.time()
+
+print("\n==> sorted in {} seconds <==".format(round(toc - tic, 2)))
---
+++ rand_generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Usage: ./rand_generator.py [amount]
+
+import sys, random, time
+
+amount = int(sys.argv[1])
+MAX_32INTEGER = 2147483647
+MIN_32INTEGER = -2147483648
+
+if len(sys.argv) > 2:
+    print("Using custom random seed: {}".format(sys.argv[2]))
+    random.seed(int(sys.argv[2]))
+
+input_file = open('input_data.txt', 'w')
+
+input_file.write(str(amount) + '\n')
+
+print('Generate {:,} random integers'.format(amount))
+tic = time.time()
+
+for i in range(amount):
+    input_file.write(str(random.randint(MIN_32INTEGER, MAX_32INTEGER)) + '\n')
+
+toc = time.time()
+
+input_file.close
+
+print("Generated in {} seconds".format(round(toc - tic, 2)))
